fa4e70daa0551fa3be0969ebfd79a494421779e5
Skyba123 <skyba.serhi@gmail.com>
2023-10-23 00:32:10 +0300

    changed LibraryTest , added dvd

--- src/test/java/org/example/LibraryTest.java
+++ src/test/java/org/example/LibraryTest.java
@@ -11,64 +11,78 @@ public class LibraryTest {
     private Item book;
     private Patron patron;
 
+    private DVD dvd;
+
     @Before
     public void setUp() {
         library = new Library();
         List<Item> borrowedItems = new ArrayList<>();
         book = new Book("Test Book", "12345", "Test Author");
         patron = new Patron("Name", "001", borrowedItems);
+        dvd = new DVD("Test DVD", "67890", 120);
     }
 
     @Test
     public void testRegisterPatron() {
         library.registerPatron(patron);
-        assertTrue(library.getPatrons().contains(patron));
+        List<Patron> registeredPatrons = library.getPatrons();
+        assertTrue(registeredPatrons.contains(patron));
+    }
+
+    @Test
+    public void testLendItem() {
+        library.registerPatron(patron);
+        library.add(book);
+        library.lendItem(patron, book);
+        assertTrue(book.getIsBorrowed());
+        List<Item> borrowedItems = patron.getBorrowedItems();
+        assertTrue(borrowedItems.contains(book));
+    }
+
+    @Test
+    public void testReturnItem() {
+        library.registerPatron(patron);
+        library.add(book);
+        library.lendItem(patron, book);
+        library.returnItem(patron, book);
+        assertFalse(book.getIsBorrowed());
+        List<Item> borrowedItems = patron.getBorrowedItems();
+        assertFalse(borrowedItems.contains(book));
     }
 
     @Test
     public void testAdd() {
         library.add(book);
-        assertTrue(library.getItems().contains(book));
+        List<Item> addedItems = library.getItems();
+        assertTrue(addedItems.contains(book));
     }
 
     @Test
     public void testRemove() {
         library.add(book);
         library.remove(book);
-        assertFalse(library.getItems().contains(book));
+        List<Item> addedItems = library.getItems();
+        assertFalse(addedItems.contains(book));
     }
 
     @Test
     public void testListAvailable() {
         library.add(book);
+        library.add(dvd);
         List<Item> availableItems = library.listAvailable();
         assertTrue(availableItems.contains(book));
+        assertTrue(availableItems.contains(dvd));
     }
 
     @Test
     public void testListBorrowed() {
         library.registerPatron(patron);
+        library.add(book);
+        library.add(dvd);
         library.lendItem(patron, book);
+        library.lendItem(patron, dvd);
         List<Item> borrowedItems = library.listBorrowed();
         assertTrue(borrowedItems.contains(book));
-    }
-
-    @Test
-    public void testLendItem() {
-        library.add(book);
-        library.registerPatron(patron);
-        library.lendItem(patron, book);
-        assertTrue(patron.getBorrowedItems().contains(book));
-        assertTrue(book.getIsBorrowed());
-    }
-
-    @Test
-    public void testReturnItem() {
-        library.add(book);
-        library.registerPatron(patron);
-        library.lendItem(patron, book);
-        library.returnItem(patron, book);
-        assertFalse(patron.getBorrowedItems().contains(book));
-        assertFalse(book.getIsBorrowed());
+        assertTrue(borrowedItems.contains(dvd));
     }
 }

8be63341a7efd9b803af859225580552cc701d03
Skyba123 <skyba.serhi@gmail.com>
2023-10-22 23:14:48 +0300

    created interface IManageable

---
+++ src/main/java/org/example/IManageable.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import java.util.List;
+
+public interface IManageable {
+    void add(Item item);
+
+    void remove(Item item);
+
+    List<Item> listAvailable();
+
+    List<Item> listBorrowed();
+}
+

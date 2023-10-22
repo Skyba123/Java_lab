daabe976661dcad77967b77480973be7fdde3f81
Skyba123 <skyba.serhi@gmail.com>
2023-10-23 01:32:43 +0300

    created class Cart(list,add/removeProduct,get)

---
+++ src/main/java/org/example/Cart.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    private List<Product> products;
+    public Cart(){
+        products = new ArrayList<>();
+    }
+    public void addProduct(Product product){
+        products.add(product);
+    }
+    public List<Product> getProducts(){
+        return products;
+    }
+    public void removeProduct(Product product){
+        products.remove(product);
+    }
+}

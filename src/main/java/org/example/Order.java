b11d27a36c5bc6affe6a31d58ab89c934d1c5c8a
Skyba123 <skyba.serhi@gmail.com>
2023-10-23 01:51:23 +0300

    changed List to ArrayList

--- src/main/java/org/example/Order.java
+++ src/main/java/org/example/Order.java
@@ -3,30 +3,33 @@ package org.example;
 
 import java.util.List;
 
+import java.util.ArrayList;
+
 public class Order {
 
     private int orderId;
-
-    private List<Product> products;
-
+    private ArrayList<Product> products;
     private String status;
 
-    public Order(int orderId, List<Product> products, String status){
-        this.orderId= orderId;
+    public Order(int orderId, ArrayList<Product> products, String status) {
+        this.orderId = orderId;
         this.products = products;
-        this.status = "Waiting";
+        this.status = status;
     }
+
     public int getOrderId() {
         return orderId;
     }
 
-    public List<Product> getProducts() {
+    public ArrayList<Product> getProducts() {
         return products;
     }
+
     public String getStatus() {
         return status;
     }
-    public void setStatus(String status){
+
+    public void setStatus(String status) {
         this.status = status;
     }
 }

053f1d52d85b9ca65aeca06a0c5fa86aab54f726
Skyba123 <skyba.serhi@gmail.com>
2023-10-23 20:32:59 +0300

    Created class PalindromeCheckerTest(testWithPalindrome,testWithNonPalindrome,testWithSingleCharacter,testWithEmptyString)

---
+++ src/main/Test/org/example/PalindromeCheckerTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PalindromeCheckerTest {
+    public void testWithPalindrome() {
+        assertTrue(PalindromeChecker.isPalindrome("level"));
+        assertTrue(PalindromeChecker.isPalindrome("refer"));
+    }
+
+    @Test
+    public void testWithNonPalindrome() {
+        assertFalse(PalindromeChecker.isPalindrome("hello"));
+        assertFalse(PalindromeChecker.isPalindrome("war"));
+    }
+    @Test
+    public void testWithSingleCharacter() {
+        assertTrue(PalindromeChecker.isPalindrome("s"));
+    }
+    @Test
+    public void testWithEmptyString() {
+        assertTrue(PalindromeChecker.isPalindrome(""));
+    }
+
+}

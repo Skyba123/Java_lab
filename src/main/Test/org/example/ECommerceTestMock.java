43a1b6f39e710dd8a6d62b7b73b6ddafeda60f57
Skyba123 <skyba.serhi@gmail.com>
2023-11-04 17:24:11 +0200

    added MockitoTest

---
+++ src/main/Test/org/example/ECommerceTestMock.java
@@ -0,0 +1,65 @@
+import org.example.Cart;
+import org.example.Order;
+import org.example.Product;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+public class ECommerceTestMock {
+
+    @Mock
+    private Product product1;
+    @Mock
+    private Product product2;
+
+    private Cart cart;
+    private Order order;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        cart = new Cart();
+        order = new Order(1, new ArrayList<>(), "Waiting");
+    }
+
+    @Test
+    public void testAddProduct() {
+        cart.addProduct(product1);
+
+        assertTrue(cart.getProducts().contains(product1));
+    }
+
+    @Test
+    public void testRemoveProduct() {
+        cart.addProduct(product1);
+        cart.removeProduct(product1);
+
+        assertFalse(cart.getProducts().contains(product1));
+    }
+
+    @Test
+    public void testPlaceOrder() {
+        cart.addProduct(product1);
+        cart.addProduct(product2);
+
+        order = new Order(1, new ArrayList<>(cart.getProducts()), "Waiting");
+
+        assertEquals("Waiting", order.getStatus());
+    }
+
+    @Test
+    public void testOrderStatus() {
+        order.setStatus("Done");
+
+        assertEquals("Done", order.getStatus());
+    }
+}

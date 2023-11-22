13e9779880934434c6d80408860444e3348faa67
Skyba123 <skyba.serhi@gmail.com>
2023-11-23 01:10:33 +0200

    created Main for checking hall

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,18 @@
 package org.example;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Cinema cinema = new Cinema(5, 10, 20);
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Бронювання місць
+        cinema.bookSeats(1, 3, new int[]{5, 6, 7,8});
+        cinema.printSeatingArrangement(1);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
-    }
+        // Скасування бронювання
+        cinema.cancelBooking(1, 3, new int[]{5, 6, 7});
+       cinema.printSeatingArrangement(1);
+        // Перевірка наявності місць
+        boolean isAvailable = cinema.checkAvailability(1,3, 8);
+        System.out.println("Availability: " + isAvailable);
+  }
 }

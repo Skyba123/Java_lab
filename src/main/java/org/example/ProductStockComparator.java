75d43f68c39620ab9372a820dbc4be263f1a23d0
Skyba123 <skyba.serhi@gmail.com>
2023-12-17 19:18:52 +0200

    create class ProductStockComparator

---
+++ src/main/java/org/example/ProductStockComparator.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.Comparator;
+
+public class ProductStockComparator implements Comparator<Product> {
+    @Override
+    public int compare(Product p1, Product p2) {
+        return Integer.compare(p1.getStock(), p2.getStock());
+    }
+}
